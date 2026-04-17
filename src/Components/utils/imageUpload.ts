07304e3d3b67403318b5b4799b4bd5523628b0ea
sujon-258549 <sujon258549@gmail.com>
2026-04-17 15:25:55 +0600

    admin

---
+++ src/Components/utils/imageUpload.ts
@@ -0,0 +1,44 @@
+import { config } from "../../config";
+
+
+// utils/cloudinary.ts
+ const uploadImageToCloudinary = async (
+  file: File,
+  cloudName: string,
+  uploadPreset: string
+) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", uploadPreset);
+
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`Upload failed: ${response.statusText}`);
+  }
+
+  return await response.json();
+};
+
+export const uploadProfileImage = async (file: File) => {
+  const cloudName =  config.cloudinary.cloudName;
+  const uploadPreset = config.cloudinary.uploadPreset;
+
+  try {
+    const uploadedData = await uploadImageToCloudinary(
+      file,
+      cloudName,
+      uploadPreset
+    );
+    return uploadedData.secure_url;
+  } catch (error) {
+    console.error("Image upload failed:", error);
+    throw error;
+  }
+};
